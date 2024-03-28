7d62a76b53e84497225596ef9f68ada27eb1cd64
Lusitha Ranathunga <rlusitha@gmail.com>
2024-03-28 22:58:31 +0530

    First commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ASP.NET_Core_Dependancy_Injection_MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
